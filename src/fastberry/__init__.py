88971f24e14ca67b9b8a68913f677486087fe4cc
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-08-03 17:33:34 -0500

    updates

--- src/fastberry/__init__.py
+++ src/fastberry/__init__.py
@@ -5,6 +5,7 @@
 from starlette.middleware.base import BaseHTTPMiddleware as BaseMiddleware
 from strawberry.extensions import Extension as BaseExtension
 from strawberry.permission import BasePermission
+import strawberry
 
 from . import config
 
@@ -16,5 +17,13 @@ from .config.extras.gql import GQL
 from .schema import Schema
 from .types import Model, SQLFilters
 
+# Custom Field Types
 Text = Model.text
 JSON = Model.json
+ID = strawberry.ID
+
+# To Camel Case
+def to_camel_case(text):
+    """Converts to Camel-Case"""
+    init, *temp = text.split("_")
+    return "".join([init.lower(), *map(str.title, temp)])
