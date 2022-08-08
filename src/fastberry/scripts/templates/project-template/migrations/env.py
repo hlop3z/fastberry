b721f91595652a055cf0d23d86bd61a3f085b69a
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-08-08 17:41:21 -0500

    alembic setup

--- src/fastberry/scripts/templates/project-template/migrations/env.py
+++ src/fastberry/scripts/templates/project-template/migrations/env.py
@@ -5,6 +5,10 @@ from sqlalchemy import pool
 
 from alembic import context
 
+# Custom Settings
+from project.databases.sql import Base
+from project.databases.sql import DATABASE_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +22,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+target_metadata = Base.metadata
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
