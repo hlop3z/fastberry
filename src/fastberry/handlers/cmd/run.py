7d077f5fd12423ea21cff6e112eeb44a1b82e95b
hlop3z <23062270+hlop3z@users.noreply.github.com>
2022-12-21 19:31:57 -0600

    v0.0.70 - dev mode run on 0.0.0.0

--- src/fastberry/handlers/cmd/run.py
+++ src/fastberry/handlers/cmd/run.py
@@ -20,10 +20,11 @@ def run(port, workers):
         - production & staging : `./manage.py run --port 8000 --workers 4`
     """
     mode = spoc.mode
+    host = "0.0.0.0"
     # FastAPI-Server
     match mode:
         case "development":
-            cmd = f"python -m uvicorn main:app --reload --port { port }"
+            cmd = f"python -m uvicorn main:app --reload --port { port } --host { host }"
         case "staging":
             cmd = f"python -m gunicorn main:app --workers { workers } --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:{ port }"
         case "production":
